f211ad8b63b7a820e6e3dd9195cb12934381b754
Kiryl21 <kirillevst1@gmail.com>
2024-09-06 22:51:59 +0200

    Complete excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(reqarray, ...rest) {
+    for (let i =0; i<rest.length; i++){
+        for(;reqarray.includes(rest[i]); ){
+        let position = reqarray.indexOf(rest[i]);
+        console.log(position);
+        reqarray.splice(position, 1)
+        }
+        }
+            return reqarray
 };
 
 // Do not edit below this line
