3f284caaceabc90fc5b7bd24b501beff20078479
Kiryl21 <kirillevst1@gmail.com>
2025-01-27 15:49:02 +0100

    Complete excercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    removed = input.toLowerCase().replace(/[!"#$%&'() *+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    arr = removed.split('')
+    reverse = arr.toReversed();
+    console.log(arr, reverse)
+    return arr.length === reverse.length && arr.every((value, index) => value === reverse[index]);
 };
 
 // Do not edit below this line
