df22d3cc4545bb1d211315e85abfb8207133524f
Kiryl21 <kirillevst1@gmail.com>
2025-01-31 15:37:15 +0100

    Complete excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    iterations = Number(input);
+    result = 1;
+    previous = 0;
+    if (iterations == 0) {
+        return 0
+    } else if (iterations < 0) {
+        return "OOPS"
+    } else {
+    for (let i = 1; i < iterations; i++){
+        result += previous;
+        previous = result - previous;
+    }
+        return result
+    }
 };
 
 // Do not edit below this line
