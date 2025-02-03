8ef965ab6abb23033fdbe9a29711c3afd2fce87b
Kiryl21 <kirillevst1@gmail.com>
2025-02-03 17:47:46 +0100

    Complete excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let fixedDate = [];
+    new Date().getFullYear()
+    arr.forEach((item) => {
+    if (item.yearOfDeath === undefined) {
+    item.yearOfDeath = new Date().getFullYear()
+    fixedDate.push(item);
+    } else {
+        fixedDate.push(item)
+    }
+    });
+    
+    let result = fixedDate.map(year => (
+    {
+        name : year.name,
+        age: year.yearOfDeath - year.yearOfBirth
+    }
+    ));
+    result.sort((a, b) => a.age - b.age )
+    return result[result.length - 1]
 };
 
 // Do not edit below this line
